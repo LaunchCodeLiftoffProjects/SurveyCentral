b5eb244c1bd96f1c086c3f784d305344f5606a0a
Nathan Gearke <n.gearke@gmail.com>
2021-01-21 20:46:31 -0600

    Options Model with properties made

---
+++ Models/OptionsModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestCentral.Models
+{
+    public class OptionsModel
+    {
+        public int Id { get; set; }
+        public int QuestionId { get; set; }
+        public string Value { get; set; }
+        public string Label { get; set; }
+
+
+    }
+}
