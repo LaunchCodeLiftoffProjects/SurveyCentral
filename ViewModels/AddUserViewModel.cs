dd55c21d61664ca232cf4d242ee1eaa02d238ac3
Angela Dudley <angela.dudley14@gmail.com>
2021-02-02 20:23:31 -0600

    Began setup for separate profile page, added side navigation bar

--- ViewModels/AddUserViewModel.cs
+++ ViewModels/AddUserViewModel.cs
@@ -8,14 +8,7 @@ namespace TestCentral.ViewModels
 {
     public class AddUserViewModel
     {
-        [Required(ErrorMessage = "E-Mail address is required.")]
-        [EmailAddress]
-        public string EmailAddress {get; set;}
-        
-        [Required(ErrorMessage = "Password is required.")]
-         public string Password { get; set; }
-
-        [Required(ErrorMessage = "First name is required.")]
+       [Required(ErrorMessage = "First name is required.")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Last name is required.")]
