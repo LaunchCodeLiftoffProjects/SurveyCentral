9f66a8d6cd97b8d7a901d1a69f674219bba23c51
Angela Dudley <angela.dudley14@gmail.com>
2021-02-03 20:55:33 -0600

    Added Profile Data

---
+++ Data/ProfileData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCentral.Models;
+
+namespace TestCentral.Data
+{
+    public class ProfileData
+    {
+        static private Dictionary<int, Profile> Profiles = new Dictionary<int, Profile>();
+
+        //getbyId
+        public static Profile GetById(int id)
+            {
+                return Profiles[id];
+            }
+    }
+}
---
+++ Data/ProfileDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestCentral.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace TestCentral.Data
+{
+    public class ProfileDbContext : DbContext
+    {
+        public DbSet<Profile> Profiles { get; set; }
+
+        public ProfileDbContext(DbContextOptions<ProfileDbContext> options) : base(options)
+        {
+        }
+    }
+}
