8b3d96e3c9ea21be4894e7464a5fea2ab77b01ec
Grace Brown <gracenbrown@sbcglobal.net>
2021-02-04 19:19:37 -0600

    Fixed issue with populating form for selected question type. Gave each input in form a unique ID.

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -6,72 +6,67 @@ function populate(s1) {
     var s1 = document.getElementById(s1);
     var s2 = document.getElementById(`restOfQuestion${questionCounter}`);
 
-    // var restOfQuestionNode = document.createElement("div");
-    // s2.appendChild(restOfQuestionNode);
-
-    if (s1.value == "trueFalse") {
-        s2.innerHTML = '<label for="trueFalse">Correct Answer:</label>' +
-            '<select name="trueFalse" id="trueFalse">' +
-            ' <option value="">Select One</option>' +
-            '  <option value="true">True</option>' +
-            '  <option value="false">False</option>' +
-            '</select>';
+    if (s1.value == `trueFalse${questionCounter}`) {
+        s2.innerHTML = `<label for='trueFalse${questionCounter}'>Correct Answer:</label>
+            <select name='trueFalse${questionCounter}' id="trueFalse${questionCounter}">
+             <option value="${questionCounter}">Select One</option>
+              <option value="true${questionCounter}">True</option>
+              <option value="false${questionCounter}">False</option>
+            </select>`;
     }
-    else if (s1.value == "multipleChoice") {
-        s2.innerHTML = '<p>Choices:</p>' +
-            '<label for="a">A</label>' +
-            '<input type="text" id="a" name="a">' +
-            '<label for="b">B</label>' +
-            '<input type="text" id="b" name="b">' +
-            '<label for="c">C</label>' +
-            '<input type="text" id="c" name="c">' +
-            '<label for="d">D</label>' +
-            '<input type="text" id="d" name="d">' +
-            '<label for="e">E</label>' +
-            '<input type="text" id="e" name="e">' + '<br>' +
-            '<label for="multipleChoice">Correct Answer:</label>' +
-            '<select name="multipleChoice" id="multipleChoice">' +
-            '  <option value="">Select One</option>' +
-            '  <option value="a">A</option>' +
-            '  <option value="b">B</option>' +
-            '  <option value="c">C</option>' +
-            '  <option value="d">D</option>' +
-            '  <option value="e">E</option>' +
-            '</select>';
+    else if (s1.value == `multipleChoice${questionCounter}`) {
+        s2.innerHTML = `<p>Choices:</p>
+            <label for="a${questionCounter}">A</label>
+            <input type="text" id="a${questionCounter}" name="a${questionCounter}">
+            <label for="b${questionCounter}">B</label>
+            <input type="text" id="b${questionCounter}" name="b${questionCounter}">
+            <label for="c${questionCounter}">C</label>
+            <input type="text" id="c${questionCounter}" name="c${questionCounter}">
+            <label for="d${questionCounter}">D</label>
+            <input type="text" id="d${questionCounter}" name="d${questionCounter}">
+            <label for="e${questionCounter}">E</label>
+            <input type="text" id="e${questionCounter}" name="e${questionCounter}"><br>
+            <label for="mulChoiceCorrect${questionCounter}">Correct Answer:</label>
+            <select name="mulChoiceCorrect${questionCounter}" id="mulChoiceCorrect${questionCounter}">
+              <option value="correct${questionCounter}">Select One</option>
+              <option value="aCorrect${questionCounter}">A</option>
+              <option value="bCorrect${questionCounter}">B</option>
+              <option value="cCorrect${questionCounter}">C</option>
+              <option value="dCorrect${questionCounter}">D</option>
+              <option value="eCorrect${questionCounter}">E</option>
+            </select>`;
     }
-    else if (s1.value == "textInput") {
-        s2.innerHTML = '<label for="input">Correct Answer:</label>' +
-            '<input type="text" id="input" name="input">';
+    else if (s1.value == `textInput${questionCounter}`) {
+        s2.innerHTML = `<label for="input${questionCounter}">Correct Answer:</label>
+            <input type="text" id="input${questionCounter}" name="input${questionCounter}">`;
     }
-    console.log(s1.value);
 }
 
 //Adds next question for user to fill out upon clicking Add Question button:
 
 function addNewQuestion() {
     questionCounter++;
-    console.log(questionCounter);
 
     let addQuestionNode = document.createElement("div");
     addQuestionNode.id = `q${questionCounter}`;
 
     addQuestionNode.innerHTML = `<h3>Question ${questionCounter}</h3>
     <div>
-        <label for="questionType">Type:</label>
-        <select name="questionType" id="questionType" onchange="populate(this.id)">
-            <option value="">Select One</option>
-            <option value="trueFalse">True/False</option>
-            <option value="multipleChoice">Multiple Choice</option>
-            <option value="textInput">Text Input</option>
+        <label for="questionType${questionCounter}">Type:</label>
+        <select name="questionType${questionCounter}" id="questionType${questionCounter}" onchange="populate(this.id)">
+            <option value="${questionCounter}">Select One</option>
+            <option value="trueFalse${questionCounter}">True/False</option>
+            <option value="multipleChoice${questionCounter}">Multiple Choice</option>
+            <option value="textInput${questionCounter}">Text Input</option>
         </select>
     </div><br>
     <div>
-        <label for="prompt">Prompt:</label>
-        <input type="text" id="prompt" name="prompt">
+        <label for="prompt${questionCounter}">Prompt:</label>
+        <input type="text" id="prompt${questionCounter}" name="prompt${questionCounter}">
     </div><br>
     <div>
-      <label for="imageLink">Add Link to Image (optional):</label>
-      <input type="text" id="imageLink" name="imageLink">
+      <label for="imageLink${questionCounter}">Add Link to Image (optional):</label>
+      <input type="text" id="imageLink${questionCounter}" name="imageLink${questionCounter}">
     </div>
     <div id="restOfQuestion${questionCounter}">
     </div>`;
@@ -79,7 +74,7 @@ function addNewQuestion() {
     document.getElementById("questionsSection").appendChild(addQuestionNode);
 }
 
-//Prevents form from sending upon click on Add Question button:
+//Prevents form from sending upon click on Add Question button
 
 window.onload = (event) => {
     document.getElementById("addQuestion").addEventListener("click", (ev) => {
@@ -87,89 +82,3 @@ window.onload = (event) => {
         addNewQuestion();
     });
 };
-
-
-
-
-//Two-arg version of populate:
-// function populate(s1, s2) {
-//     var s1 = document.getElementById(s1);
-//     var s2 = document.getElementById(s2);
-//     s2.innerHTML = "";
-
-//     if (s1.value == "trueFalse") {
-//         s2.innerHTML = '<label for="trueFalse">Correct Answer:</label>' +
-//             '<select name="trueFalse" id="trueFalse">' +
-//             ' <option value="">Select One</option>' +
-//             '  <option value="true">True</option>' +
-//             '  <option value="false">False</option>' +
-//             '</select>';
-//     }
-//     else if (s1.value == "multipleChoice") {
-//         s2.innerHTML = '<p>Choices:</p>' +
-//             '<label for="a">A</label>' +
-//             '<input type="text" id="a" name="a">' +
-//             '<label for="b">B</label>' +
-//             '<input type="text" id="b" name="b">' +
-//             '<label for="c">C</label>' +
-//             '<input type="text" id="c" name="c">' +
-//             '<label for="d">D</label>' +
-//             '<input type="text" id="d" name="d">' +
-//             '<label for="e">E</label>' +
-//             '<input type="text" id="e" name="e">' + '<br>' +
-//             '<label for="multipleChoice">Correct Answer:</label>' +
-//             '<select name="multipleChoice" id="multipleChoice">' +
-//             '  <option value="">Select One</option>' +
-//             '  <option value="a">A</option>' +
-//             '  <option value="b">B</option>' +
-//             '  <option value="c">C</option>' +
-//             '  <option value="d">D</option>' +
-//             '  <option value="e">E</option>' +
-//             '</select>';
-//     }
-//     else if (s1.value == "textInput") {
-//         s2.innerHTML = '<label for="input">Correct Answer:</label>' +
-//             '<input type="text" id="input" name="input">';
-//     }
-// }
-
-//Former version of addNewQuestion using innerHTML, not appendChild:
-//let newQuestionContent;
-// function addNewQuestion() {
-//   var questionsSection = document.getElementById('questionsSection');
-//   questionsSection.innerHTML += `<div id="question">
-//     <h3>Question ${questionCounter}</h3>
-//     <!-- <div>
-//         <button id="button-edit">Edit</button>
-//         <button id="button-delete">Delete</button>
-//     </div> -->
-//     <div>
-//         <label for="questionType">Type:</label>
-//         <select name="questionType" id="questionType" onchange="populate(this.id, 'restOfQuestion')">
-//             <option value="">Select One</option>
-//             <option value="trueFalse">True/False</option>
-//             <option value="multipleChoice">Multiple Choice</option>
-//             <option value="textInput">Text Input</option>
-//         </select>
-//     </div><br>
-//     <div>
-//         <label for="prompt">Prompt:</label>
-//         <input type="text" id="prompt" name="prompt">
-//         </div><br>
-//         <div>
-//           <label for="imageLink">Add Link to Image (optional):</label>
-//           <input type="text" id="imageLink" name="imageLink">
-//         </div>
-//         <div id="restOfQuestion">
-//         </div>
-//       </div>`;
-//     questionCounter++;
-// }
-
-//dynamic click options:
-//document.getElementById("addQuestion").addEventListener("click", function () {
-//    var questionsSection = document.getElementById('questionsSection');
-//    questionsSection.innerHTML += newQuestionContent;
-//});
-
-//document.getElementById("addQuestion").addEventListener("click", addNewQuestion());
