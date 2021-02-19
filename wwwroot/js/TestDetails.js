228eda05ba2a1ee8feefca2992827281699658e0
Nathan Gearke <n.gearke@gmail.com>
2021-02-18 20:33:47 -0600

    Added Print Test button, moved JS code to its own file, and added question type label.

---
+++ wwwroot/js/TestDetails.js
@@ -0,0 +1,23 @@
+﻿// gets a div and prints only that section to avoid printing the buttons
+function printDiv(divName) {
+    var printContents = document.getElementById(divName).innerHTML;
+    var originalContents = document.body.innerHTML;
+
+    document.body.innerHTML = printContents;
+
+    window.print();
+
+    document.body.innerHTML = originalContents;
+}
+
+// Erases correct answers, and formats test details for printing or displaying in the Test Details page
+function printableTest(questionsCount) {
+    document.getElementById("testDates").remove();
+    document.getElementById("name").innerHTML = "Name: ________________________________________";
+
+    let answers = document.getElementsByClassName("answers");
+    for (let i = 0; i < questionsCount; i++) {
+        answers[i].classList.add('hiddenForPrinting');
+    }
+}
+
