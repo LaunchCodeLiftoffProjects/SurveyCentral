6d64f839770ae1ca1222c9f1265f1d8b1f9128fe
Nathan Gearke <n.gearke@gmail.com>
2021-02-25 20:58:40 -0600

    Now can only populate answers and made them bigger. Also added a refresh button.

--- wwwroot/js/TestDetails.js
+++ wwwroot/js/TestDetails.js
@@ -4,20 +4,29 @@ function printDiv(divName) {
     var originalContents = document.body.innerHTML;
 
     document.body.innerHTML = printContents;
-
     window.print();
-
     document.body.innerHTML = originalContents;
 }
 
 // Erases correct answers, and formats test details for printing or displaying in the Test Details page
-function printableTest(questionsCount) {
+function printableTest() {
     document.getElementById("testDates").remove();
     document.getElementById("name").innerHTML = "Name: ________________________________________";
 
     let answers = document.getElementsByClassName("answers");
-    for (let i = 0; i < questionsCount; i++) {
-        answers[i].classList.add('hiddenForPrinting');
+    for (let i = 0; i < answers.length; i++) {
+        answers[i].innerHTML="";
+    }
+}
+
+//Print only the answers
+function printableKey() {
+    let questionsPrompt = document.getElementsByClassName("questionsPromptArea");
+
+
+    for (let i = 0; i < questionsPrompt.length; i++) {
+
+        questionsPrompt[i].innerHTML = "";
     }
 }
 
@@ -39,3 +48,5 @@ function postDeleteQuestion(questionId) {
     }
     return;
 }
+
+
