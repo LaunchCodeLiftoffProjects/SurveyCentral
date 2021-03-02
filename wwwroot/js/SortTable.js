d625af581980643a16c1921e128e0a02d511df39
Nathan Gearke <n.gearke@gmail.com>
2021-03-01 23:08:58 -0600

    Added a search bar for the test name

--- wwwroot/js/SortTable.js
+++ wwwroot/js/SortTable.js
@@ -1,6 +1,6 @@
 ﻿function sortTable(n) {
     var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-    table = document.getElementById("sortableTable");
+    table = document.getElementById("myTable");
     switching = true;
     //Set the sorting direction to ascending:
     dir = "ASC";
---
+++ wwwroot/js/searchTable.js
@@ -0,0 +1,27 @@
+﻿function searchTable() {
+    // Declare variables
+    var input;
+    var filter;
+    var table;
+    var tr;
+    var td;
+    var i;
+    var txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("sortableTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+}
